503ed67c8dcb00d249314e399a54f27a4ba677b5
Anton Kosterin <Antonyan.Kosterko@mail.ru>
2025-03-04 17:42:02 +0300

    add lab3

---
+++ pages/main/index.js
@@ -0,0 +1,72 @@
+import {AccordionComponent} from "../../components/accordion/index.js";
+import {ProductPage} from "../product/index.js";
+
+export class MainPage {
+  constructor(parent) {
+    this.parent = parent;
+  }
+
+  getData() {
+    return [
+      {
+        title: 'Golden Retriever',
+        content: `
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0gcUu-D8ElLcFw0TZSvCdBasqs4rG_YesHQm9Rw269lUZeRMN-dPzVZ9VH82U9Ji8OC3XHBke75d21Coy7Cho4Q"
+            class="img-fluid"
+            alt="Golden Retriever"
+          />
+          <p>Golden Retrievers are friendly, intelligent, and devoted.</p>
+        `
+      },
+      {
+        title: 'German Shepherd',
+        content: `
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg"
+            class="img-fluid"
+            alt="German Shepherd"
+          />
+          <p>German Shepherds are known for their intelligence and loyalty.</p>
+        `
+      },
+      {
+        title: 'Bulldog',
+        content: `
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bulldog_inglese.jpg/800px-Bulldog_inglese.jpg"
+            class="img-fluid"
+            alt="Bulldog"
+          />
+          <p>Bulldogs are calm and friendly, with a distinctive wrinkled face.</p>
+        `
+      }
+    ];
+  }
+
+  get pageRoot() {
+    return document.getElementById('main-page');
+  }
+
+  clickCard(index, e) {
+    const cardId = e.target.dataset.id;
+    const productPage = new ProductPage(this.parent, index);
+    productPage.render();
+  }
+      
+  getHTML() {
+    return `<div id="main-page" class="d-flex flex-wrap"></div>`;
+  }
+      
+  render() {
+    this.parent.innerHTML = '';
+    const html = this.getHTML();
+    this.parent.insertAdjacentHTML('beforeend', html);
+    
+    const data = this.getData();
+    data.forEach((item, index) => {
+      const accordion = new AccordionComponent(this.pageRoot);
+      accordion.render([item], `accordion-${index}`, this.clickCard.bind(this, index));
+    });
+  }
+}
---
+++ pages/product/index.js
@@ -0,0 +1,65 @@
+import {ProductComponent} from "../../components/product/index.js";
+import {BackButtonComponent} from "../../components/back-button/index.js";
+import {MainPage} from "../main/index.js";
+
+export class ProductPage {
+    constructor(parent, id) {
+        this.parent = parent
+        this.id = id
+    }
+
+    getData() {
+        const products = {
+            0: {
+                src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0gcUu-D8ElLcFw0TZSvCdBasqs4rG_YesHQm9Rw269lUZeRMN-dPzVZ9VH82U9Ji8OC3XHBke75d21Coy7Cho4Q",
+                title: "Golden Retriever",
+                text: "Golden Retrievers are friendly, intelligent, and devoted. They are one of the most popular dog breeds."
+            },
+            1: {
+                src: "https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg",
+                title: "German Shepherd",
+                text: "German Shepherds are known for their intelligence, courage, and loyalty. They are often used in police and military roles."
+            },
+            2: {
+                src: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Bulldog_inglese.jpg/800px-Bulldog_inglese.jpg",
+                title: "Bulldog",
+                text: "Bulldogs are calm, courageous, and friendly. They are known for their wrinkled face and muscular build."
+            }
+        };
+
+        return products[this.id] || {};
+    }
+
+    get pageRoot() {
+        return document.getElementById('product-page')
+    }
+
+    getHTML() {
+        return `
+            <div id="product-page" class="class="d-flex flex-column justify-content-center align-items-center" style="min-height: 100vh;""></div>
+        `;
+    }
+
+    clickBack() {
+        const mainPage = new MainPage(this.parent);
+        mainPage.render();
+    }
+
+    render() {
+        this.parent.innerHTML = '';
+        const html = this.getHTML();
+        this.parent.insertAdjacentHTML('beforeend', html);
+
+        const data = this.getData();
+
+        if (!data || !data.title) {
+            this.pageRoot.innerHTML = `<p>Продукт не найден</p>`;
+            return;
+        }
+
+        const product = new ProductComponent(this.pageRoot);
+        product.render(data);
+        const backButton = new BackButtonComponent(this.pageRoot);
+        backButton.render(this.clickBack.bind(this));
+    }
+}
